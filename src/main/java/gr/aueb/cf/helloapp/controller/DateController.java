3a37eb3212b3299da0b590100ed587c00ddfd254
Nick Dimizas <nikkosdim@gmail.com>
2025-03-28 21:15:26 +0200

    landing and teachers page and date-controller created

---
+++ src/main/java/gr/aueb/cf/helloapp/controller/DateController.java
@@ -0,0 +1,29 @@
+package gr.aueb.cf.helloapp.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+@WebServlet("/date")
+public class DateController extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, dd MM yyyy HH:mm:ss", Locale.forLanguageTag("el"));
+        String currentDateTimeStr = currentDateTime.format(formatter);
+
+        request.setAttribute("currentDateTimeStr", currentDateTimeStr);
+        request.getRequestDispatcher("WEB-INF/jsp/date.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/date.jsp
@@ -0,0 +1,8 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+
+<html>
+<body>
+<h1>Ημερομηνία και Ώρα</h1>
+<h2>${requestScope.currentDateTimeStr}</h2>
+</body>
+</html>
